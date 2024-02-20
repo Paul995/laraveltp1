e7bb5202e2db3fa05009913a50ae27eece702ee0
Your Name <pwdawson90@gmail.com>
2024-02-20 18:34:41 -0500

    finaliser edit

--- Maisonneuve2395529/resources/views/edit.blade.php
+++ Maisonneuve2395529/resources/views/edit.blade.php
@@ -26,33 +26,30 @@
                     </div>
 
                     <div class="mb-3">
-                       <label for="adresse" class="form-label">Adresse</label>        <!-- pour conserver les champs si erreurs -->
+                       <label for="adresse" class="form-label">Adresse</label>      
                         <input type="text" name="adresse" id="adresse" class="form-control" value="{{old('adresse', $etudiant->adresse)}}">
                         @if($errors->has('adresse'))
                         <div class="text-danger mt2-2">
-                            <!-- tableau dans un tableau ex: errors[title[...],...] -->
                             {{$errors->first('adresse')}}
                         </div>
                         @endif
                     </div>
 
                     <div class="mb-3">
-                       <label for="telephone" class="form-label">telephone</label>        <!-- pour conserver les champs si erreurs -->
+                       <label for="telephone" class="form-label">telephone</label>     
                         <input type="text" name="telephone" id="telephone" class="form-control" value="{{old('telephone', $etudiant->telephone)}}">
                         @if($errors->has('telephone'))
                         <div class="text-danger mt2-2">
-                            <!-- tableau dans un tableau ex: errors[title[...],...] -->
                             {{$errors->first('telephone')}}
                         </div>
                         @endif
                     </div>
 
                     <div class="mb-3">
-                       <label for="email" class="form-label">email</label>        <!-- pour conserver les champs si erreurs -->
+                       <label for="email" class="form-label">email</label>     
                         <input type="text" name="email" id="email" class="form-control" value="{{old('email', $etudiant->email)}}">
                         @if($errors->has('email'))
                         <div class="text-danger mt2-2">
-                            <!-- tableau dans un tableau ex: errors[title[...],...] -->
                             {{$errors->first('email')}}
                         </div>
                         @endif
@@ -64,8 +61,17 @@
                         <input type="date" name="date_de_naissance" id="date_de_naissance" class="form-control" value="{{old('date_de_naissance', $etudiant->date_de_naissance)}}">
                         @if($errors->has('date_de_naissance'))
                         <div class="text-danger mt2-2">
-                            <!-- tableau dans un tableau ex: errors[title[...],...] -->
                             {{$errors->first('date_de_naissance')}}
+                        </div>
+                        @endif
+                    </div>
+
+                    <div class="mb-3">
+                       <label for="ville_id" class="form-label">ville</label>        <!-- pour conserver les champs si erreurs -->
+                        <input type="number" min="1" max="15" name="ville_id" id="ville_id" class="form-control" value="{{old('ville_id', $etudiant->ville_id)}}">
+                        @if($errors->has('ville_id'))
+                        <div class="text-danger mt2-2">
+                            {{$errors->first('ville_id')}}
                         </div>
                         @endif
                     </div>
